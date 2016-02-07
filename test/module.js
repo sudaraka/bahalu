89dadf552e03f8b600c28634494203943249da87
Sudaraka Wijesinghe <sudaraka@sudaraka.org>
2016-02-07 11:07:28 +0530

    test: ensure correct objects are exported by main module

---
+++ test/module.js
@@ -0,0 +1,32 @@
+/**
+ * test/test.js: sample test spec
+ *
+ * Copyright 2016 Sudaraka Wijesinghe <sudaraka@sudaraka.org>
+ *
+ * This program comes with ABSOLUTELY NO WARRANTY;
+ * This is free software, and you are welcome to redistribute it and/or modify
+ * it under the terms of the BSD 2-clause License. See the LICENSE file for more
+ * details.
+ *
+ */
+
+import expect from 'expect';
+import * as containers from '../src';
+
+describe('Bahalu Module', () => {
+
+  it('should be an Object', () => {
+    expect(containers).toBeAn(Object);
+  });
+
+  describe('Either pattern Containers', () => {
+
+    it('should include "right" container', () => {
+      expect(Object.keys(containers)).toInclude('right');
+      expect(containers.right).toBeAn(Function);
+      expect(containers.right().constructor.name).toBe('Right');
+    });
+
+  });
+
+});
