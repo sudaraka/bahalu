40a8005c97154956c4f295d41ccf635630c80a69
Sudaraka Wijesinghe <sudaraka@sudaraka.org>
2016-08-15 00:53:24 +0530

    Test: [utilities] add test for utility functions
    Adding a test file for utility functions. At the moment it only contain the test
    for `freeze` function.

---
+++ src/utils_spec.js
@@ -0,0 +1,51 @@
+/**
+ * src/utild_spec.js: tests for utility functions
+ *
+ * Copyright 2016 Sudaraka Wijesinghe <sudaraka@sudaraka.org>
+ *
+ * This program comes with ABSOLUTELY NO WARRANTY
+ * This is free software, and you are welcome to redistribute it and/or modify
+ * it under the terms of the BSD 2-clause License. See the LICENSE file for more
+ * details.
+ *
+ */
+
+import expect from 'expect'
+import { freeze } from './utils'
+
+describe('Utilities', () => {
+
+  describe('freeze()', () => {
+
+    it('should freeze simple object', () => {
+      const
+        obj = freeze({})
+
+      expect(Object.isFrozen(obj)).toBe(true)
+    })
+
+    it('should freeze object property (value)', () => {
+      const
+        obj = freeze({ 'prop': 42 })
+
+      expect(Object.isFrozen(obj.prop)).toBe(true)
+    })
+
+    it('should freeze object nested object', () => {
+      const
+        obj1 = freeze({ 'prop': {} }),
+        obj2 = freeze({ 'prop1': { 'prop2': {} } })
+
+      expect(Object.isFrozen(obj1.prop)).toBe(true)
+      expect(Object.isFrozen(obj2.prop1.prop2)).toBe(true)
+    })
+
+    it('should freeze object method', () => {
+      const
+        obj = freeze({ 'func': () => 42 })
+
+      expect(Object.isFrozen(obj.func)).toBe(true)
+    })
+
+  })
+})
